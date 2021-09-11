1fa1ae474caf8d3f2ada0d154257effe7293d53d
Anuj Tyagi <tyagi.an@husky.neu.edu>
2021-09-11 18:43:39 -0400

    Update log_analysis.py

--- log_analysis.py
+++ log_analysis.py
@@ -1,7 +1,6 @@
 import timeit
 from collections import defaultdict 
 def analyze_logs(input_time1:float, input_time2:float, input_filename: str) -> str:
-
     print ("Between time", input_time1," & ",input_time2,":")
     total_request = 0
     fail_req = 0 
@@ -10,13 +9,17 @@ def analyze_logs(input_time1:float, input_time2:float, input_filename: str) -> s
         domain_status = defaultdict(int)
         http_status = 500
         for line in lines:
-            #print (line)
+            
+            # 
             if  (float(line.split('|')[0]) > input_time1 and float(line.split('|')[0]) < input_time2):
                 total_request += 1
+                
+                # line.split('|')[4] is http status 
                 if int(line.split('|')[4]) == http_status:
                     fail_req += 1
-                    domain_status[str(line.split('|')[2]).strip()] = (fail_req/total_request)*100
-                #domain_status[line.split('|')[2]] += total_request
+                # line.split('|')[2] is hostname/domain-name
+                domain_status[str(line.split('|')[2]).strip()] = (fail_req/total_request)*100
+
     return domain_status
 input_time1 = 1493969101.639
 input_time2 = 1493969101.660
